201e9a11bef4bcad3a831811f58a31b9fa677f76
matthieumoreau0 <matthieu.moreau@eleves.enpc.fr>
2021-04-17 01:00:24 +0200

    Setup json input parsing

--- arbre.h
+++ arbre.h
@@ -6,18 +6,19 @@
 #include <vector>
 #include <map>
 #include <limits>
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 using namespace std;
 
 // Constant globale en position value =0:
 
 // 1 Case :
 
-const int NW =-6;
-const int NE =-5;
-const int SE =+5;
-const int SW =+4;
-
-
+const int NW = -6;
+const int NE = -5;
+const int SE = +5;
+const int SW = +4;
 
 // 2 Cases :
 
@@ -33,112 +34,119 @@ class King;
 class Board;
 class Move;
 
-
-
-class Piece {
+class Piece
+{
 protected:
     int position;
     std::string color;
+
 public:
     //Defined Function :
-    virtual ~Piece(){}
-    Piece(int pos, std::string col){
+    virtual ~Piece() {}
+    Piece(int pos, std::string col)
+    {
         position = pos;
         color = col;
     }
-    Piece(const Piece& p){
+    Piece(const Piece &p)
+    {
         position = p.position;
         color = p.color;
     }
-    int getPosition(){return position;}
-    void setPosition(int pos){position = pos;}
-    std::string Color(){return color;}
-    virtual bool isMan() const =0;
-    virtual bool isKing() const {return !isMan();}
-    virtual void killFreeMove(Board& B, vector<Move> &possibleMoves) =0;
-    virtual void killingMove(Board& B, vector<Move> &possibleMoves)=0;
-    virtual void select(Board& B,vector<Move> &possibleMoves)=0;
-    virtual Piece* clone()=0;
+    int getPosition() { return position; }
+    void setPosition(int pos) { position = pos; }
+    std::string Color() { return color; }
+    virtual bool isMan() const = 0;
+    virtual bool isKing() const { return !isMan(); }
+    virtual void killFreeMove(Board &B, vector<Move> &possibleMoves) = 0;
+    virtual void killingMove(Board &B, vector<Move> &possibleMoves) = 0;
+    virtual void select(Board &B, vector<Move> &possibleMoves) = 0;
+    virtual Piece *clone() = 0;
 };
 
-class Man : public Piece {
+class Man : public Piece
+{
 public:
-    ~Man(){}
-    Man(int pos, std::string col) : Piece(pos,col){}
-    Man(const Man& m) : Piece(m.position, m.color){}
-    virtual bool isMan() const { return true;}
-    virtual void killFreeMove(Board& B,vector<Move> &possibleMoves);
-    virtual void killingMove(Board& B, vector<Move> &possibleMoves);
-    virtual void select(Board& B, vector<Move> &possibleMoves);
-    virtual Piece* clone();
+    ~Man() {}
+    Man(int pos, std::string col) : Piece(pos, col) {}
+    Man(const Man &m) : Piece(m.position, m.color) {}
+    virtual bool isMan() const { return true; }
+    virtual void killFreeMove(Board &B, vector<Move> &possibleMoves);
+    virtual void killingMove(Board &B, vector<Move> &possibleMoves);
+    virtual void select(Board &B, vector<Move> &possibleMoves);
+    virtual Piece *clone();
 };
 
-class Move{
+class Move
+{
 private:
     vector<int> path;
     int kills;
+
 public:
-    Move() {
-        kills=-1;
+    Move()
+    {
+        kills = -1;
     }
-    Move(const Move& m);
-    Move(int s, int a, int k){
+    Move(const Move &m);
+    Move(int s, int a, int k)
+    {
         path.push_back(s);
         path.push_back(a);
         kills = k;
     }
-    Move(vector<int> path0,int nbKills) {
-    	path=path0;
-    	kills=nbKills;
+    Move(vector<int> path0, int nbKills)
+    {
+        path = path0;
+        kills = nbKills;
     }
-    void operator=(const Move& m);
-    bool operator==(Move& m);
-    bool operator<(const Move& m) const;
-    int getStart() {return path[0];}
-    int getArrival() {return path[path.size()-1];}
-    int getKills() const {return kills;}
-    vector<int> getPath() const {return path;}
+    void operator=(const Move &m);
+    bool operator==(Move &m);
+    bool operator<(const Move &m) const;
+    int getStart() { return path[0]; }
+    int getArrival() { return path[path.size() - 1]; }
+    int getKills() const { return kills; }
+    vector<int> getPath() const { return path; }
     Move extendMove(Move m);
-
 };
 
+class Board
+{
+    std::vector<Piece *> pieces;
 
-class Board{
-    std::vector<Piece*> pieces;
-public :
+public:
     Board();
+    Board(const json &positions);
     ~Board();
-    Board(const Board& b);
-    void operator=(const Board& b);
+    Board(const Board &b);
+    void operator=(const Board &b);
     int index_man_here(int pos);
     bool isManHere(int pos);
     bool isKingHere(int pos);
     bool isPieceHere(int pos);
-    void playMove(const Move& m, bool inSelect=false);
+    void playMove(const Move &m, bool inSelect = false);
     void killAt(int pos);
-    Piece* getPiece(int index){return pieces[index];}
+    Piece *getPiece(int index) { return pieces[index]; }
     void turnToKing(int pos);
-    int nbPieces() const{return pieces.size();}
-    float evaluateBetter(float manWeight, float kingWeight,float nbMoveWeight, float advancementForwardWeight, float centralWeight, string color);
-    std::pair<float,Move> bestMoveAlphaBeta2(string color,int depth, float manWeight, float kingWeight,float nbMoveWeight, float centralWeight, float advanceWeight, bool maxNode,float alpha, float beta );
-    map<int,vector<Move> > playableMoves(string color);
+    int nbPieces() const { return pieces.size(); }
+    float evaluateBetter(float manWeight, float kingWeight, float nbMoveWeight, float advancementForwardWeight, float centralWeight, string color);
+    std::pair<float, Move> bestMoveAlphaBeta2(string color, int depth, float manWeight, float kingWeight, float nbMoveWeight, float centralWeight, float advanceWeight, bool maxNode, float alpha, float beta);
+    map<int, vector<Move>> playableMoves(string color);
     bool endGame();
     int timeMatch();
 };
 
-
-
-class King : public Piece {
-public :
-        ~King(){}
-        virtual bool isMan() const {return false;}
-        King(int pos, std::string col) : Piece(pos,col){}
-        King(const King& k) : Piece(k.position, k.color){}
-        virtual void killFreeMove(Board& B,vector<Move> &possibleMoves);
-        virtual void killingMove(Board& B, vector<Move> &possibleMoves);
-        virtual Piece* clone();
-        virtual void select(Board& b, vector<Move> &possibleMoves);
-
+class King : public Piece
+{
+public:
+    ~King() {}
+    virtual bool isMan() const { return false; }
+    King(int pos, std::string col) : Piece(pos, col) {}
+    King(const King &k) : Piece(k.position, k.color) {}
+    virtual void killFreeMove(Board &B, vector<Move> &possibleMoves);
+    virtual void killingMove(Board &B, vector<Move> &possibleMoves);
+    virtual Piece *clone();
+    virtual void select(Board &b, vector<Move> &possibleMoves);
 };
 
-Move getSecond(pair<float, Move> A );
+Move getSecond(pair<float, Move> A);
--- test.cpp
+++ test.cpp
@@ -1,27 +1,37 @@
 #include "arbre.h"
-#include <string> 
-#include <nlohmann/json.hpp>
+#include <string>
+#include "nlohmann/json.hpp"
 
 // for convenience
 using json = nlohmann::json;
 
-void to_json(json& j, const Move& m) {
-        j = json{{"path", m.path}, {"kills", m.kills}};
-    }
-
-void from_json(const json& j, Move& m) {
-    j.at("path").get_to(m.path);
-    j.at("kills").get_to(m.kills);
+void to_json(json &j, const Move &m)
+{
+    j = json{{"path", m.getPath()}, {"kills", m.getKills()}};
 }
 
-int main(){
-    Board B;
-   map<int,vector<Move> > currentPlayableMove = B.playableMoves("white");
-   json playableMovesJson(currentPlayableMove);
-   std::cout<<playableMovesJson.dump(4);
-//    map<int,vector<Move> >::iterator it = currentPlayableMove.begin();
-//    int arrival=(*it).second[0].getArrival();
-//    std::string s = std::to_string(arrival);
-//    std::cout<<s;
-   return 0;
+void from_json(const json &j, Move &m)
+{
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << argc << std::endl;
+    std::cout << argv[1] << std::endl;
+    json positions;
+    try
+    {
+        positions = json::parse(argv[1]);
+    }
+    catch (json::exception &e)
+    {
+        std::cout << "Error while parsing input json : " << e.what() << '\n';
+    }
+
+    Board B(positions);
+    map<int, vector<Move>> currentPlayableMove = B.playableMoves("black");
+    json playableMovesJson(currentPlayableMove);
+
+    std::cout << playableMovesJson.dump(4) << std::endl;
+    return 0;
 }
